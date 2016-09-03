feafc5b6375e18d3c2f124e360be9825c481a5be
tiagomartinho <tiago.martinho@xpeppers.com>
2016-09-03 12:32:53 -0400

    prints temperature value every second

--- Eve/ViewController.swift
+++ Eve/ViewController.swift
@@ -5,25 +5,42 @@ class ViewController: UIViewController, HMHomeManagerDelegate {
 
     var homeManager: HMHomeManager?
 
+    var temperatureCharacteristic: HMCharacteristic?
+
+    var timer: Timer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         homeManager = HMHomeManager()
         homeManager?.delegate = self
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+            self.temperatureCharacteristic?.readValue { _ in
+                print(self.temperatureCharacteristic?.value)
+            }
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer?.invalidate()
+        timer = nil
+    }
+
     func homeManagerDidUpdateHomes(_ manager: HMHomeManager) {
-        print("homeManagerDidUpdateHomes")
-    }
-
-    func homeManagerDidUpdatePrimaryHome(_ manager: HMHomeManager) {
-        print("homeManagerDidUpdatePrimaryHome")
-    }
-
-    func homeManager(_ manager: HMHomeManager, didAdd home: HMHome) {
-        print("homeManagerdidAddhome")
-    }
-
-    func homeManager(_ manager: HMHomeManager, didRemove home: HMHome) {
-        print("homeManagerdidRemovehome")
+        for home in manager.homes {
+            for accessorie in home.accessories {
+                for service in accessorie.services {
+                        for characteristic in service.characteristics {
+                            if characteristic.localizedDescription.contains("Temperature") {
+                                temperatureCharacteristic = characteristic
+                        }
+                    }
+                }
+            }
+        }
     }
 }
